bd690336a688a5957851583326c84bb2d1d07940
Shubham <shm.code1@gmail.com>
2024-01-30 22:37:52 +0530

    150-Evaluate-Reverse-Polish-Notation

---
+++ 2024-1-30/150-Evaluate-Reverse-Polish-Notation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        for(int i=0;i<tokens.size();i++){
+            if(tokens[i]=="+"||tokens[i]=="-"||tokens[i]=="*"||tokens[i]=="/"){
+                int y=st.top();
+                st.pop();
+                int x=st.top();
+                st.pop();
+                int z;
+                if(tokens[i]=="+") z=x+y;
+                else if(tokens[i]=="-") z=x-y;
+                else if(tokens[i]=="*") z=x*y;
+                else{
+                    z=x/y;
+                }
+                st.push(z);
+            }
+            else{
+                st.push(stoi(tokens[i]));  // stoi converts string to integer
+            }
+        }
+        return st.top();
+    }
+};
